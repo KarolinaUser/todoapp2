0d0426868991fbec379fb1ef89afe5b252b1ae12
KarolinaUser <karolina.krakow@o2.pl>
2022-04-24 15:59:49 +0200

    Instalacja Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todoapp2-f4299',
+    appId: '1:37824696185:web:b4a72bb9609df227f5e9a8',
+    storageBucket: 'todoapp2-f4299.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAPXDGGHHHPic6XyPvvDsMq20Rtl04xGlY',
+    authDomain: 'todoapp2-f4299.firebaseapp.com',
+    messagingSenderId: '37824696185',
+  },
   production: true
 };
