32649b04519dcca7022bf9d45a781b03a7149b4f
KarolinaUser <karolina.krakow@o2.pl>
2022-04-25 13:12:49 +0200

    add task component + form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageModule } from './pages/home.page-module';
 import { TaskPageModule } from './pages/task.page-module';
-import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [{ 
         path: 'home', 
@@ -12,9 +11,7 @@ const routes: Routes = [{
         path: 'task', 
         loadChildren: () => TaskPageModule
       },
-      { 
-        path: 'todos', component: TodosComponent
-      }
+
 ];
 
 @NgModule({
